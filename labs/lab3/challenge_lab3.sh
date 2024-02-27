f2ed71b59f248110b12c14b9fea9169fe574b421
Snakie99 <rdeida@students.pccc.edu>
2024-02-26 21:35:03 -0500

    lab3 finished

---
+++ labs/lab3/challenge_lab3.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Author: Romelo Deida
+#Date: current date in format 02/26/2024
+#Description: This is a basic script that
+#uses environment variables the date, uname, and echo commands
+echo "This current date is:"
+date --rfc-email
+echo " "
+echo "Some information about my system:"
+uname -a
+echo " "
+echo "Ram Information"
+free
+echo " "
+echo "Challenge Question Complete"
+
---
+++ labs/lab3/lab3.md
@@ -0,0 +1,21 @@
+---
+Student: Romelo
+semester: Spring 24
+Assignment: Lab 3
+---
+
+# Lab 3 Submission
+
+## Question 1
+N/A
+## Question 2
+![Question2_Image](Question2.png)
+
+## Question 3
+![Question3_Image](Question3.png)
+
+## Challenge Question
+![Question4_Image](Question4.png)
+
+## Script Source code:
+[lab3](challenge_lab3.sh)
